1d404cfbf2cdeaf45a74c881c2994057aa7e085b
LABORATORIO A <LABORATORIO A@LAB.EDU.BO>
2024-03-26 10:22:55 -0800

    ojo

---
+++ SisteamasVentas/SistemasVentas.VISTA/InterfazGerenteVista/InterfazGerenteMenu.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace SistemasVentas.VISTA.InterfazGerenteVista
+{
+    partial class InterfazGerenteMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InterfazGerenteMenu));
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            panel3 = new Panel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.RoyalBlue;
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(-2, -2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(960, 186);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(14, 11);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(131, 97);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Sitka Text", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(159, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(163, 50);
+            label2.TabIndex = 2;
+            label2.Text = "VENTAS";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Sitka Text", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(140, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(182, 50);
+            label1.TabIndex = 1;
+            label1.Text = "SISTEMA";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 88);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(957, 95);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.AutoScroll = true;
+            panel2.BackColor = Color.RoyalBlue;
+            panel2.Controls.Add(flowLayoutPanel1);
+            panel2.Location = new Point(-2, 187);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(309, 519);
+            panel2.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(button1);
+            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Controls.Add(button3);
+            flowLayoutPanel1.Controls.Add(button4);
+            flowLayoutPanel1.Controls.Add(button5);
+            flowLayoutPanel1.Controls.Add(button6);
+            flowLayoutPanel1.Controls.Add(button7);
+            flowLayoutPanel1.Controls.Add(button8);
+            flowLayoutPanel1.Controls.Add(button9);
+            flowLayoutPanel1.Controls.Add(button10);
+            flowLayoutPanel1.Controls.Add(button11);
+            flowLayoutPanel1.Controls.Add(button12);
+            flowLayoutPanel1.Controls.Add(button13);
+            flowLayoutPanel1.Controls.Add(button14);
+            flowLayoutPanel1.Controls.Add(button15);
+            flowLayoutPanel1.Controls.Add(button16);
+            flowLayoutPanel1.Location = new Point(14, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(274, 691);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Azure;
+            button1.Font = new Font("Sitka Text", 11.249999F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(254, 34);
+            button1.TabIndex = 0;
+            button1.Text = "PERSONA";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(3, 43);
+            button2.Name = "button2";
+            button2.Size = new Size(254, 36);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(3, 85);
+            button3.Name = "button3";
+            button3.Size = new Size(254, 36);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(3, 127);
+            button4.Name = "button4";
+            button4.Size = new Size(254, 36);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(3, 169);
+            button5.Name = "button5";
+            button5.Size = new Size(254, 36);
+            button5.TabIndex = 4;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(3, 211);
+            button6.Name = "button6";
+            button6.Size = new Size(254, 36);
+            button6.TabIndex = 5;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(3, 253);
+            button7.Name = "button7";
+            button7.Size = new Size(254, 36);
+            button7.TabIndex = 6;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(3, 295);
+            button8.Name = "button8";
+            button8.Size = new Size(254, 36);
+            button8.TabIndex = 7;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(3, 337);
+            button9.Name = "button9";
+            button9.Size = new Size(254, 36);
+            button9.TabIndex = 8;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(3, 379);
+            button10.Name = "button10";
+            button10.Size = new Size(254, 36);
+            button10.TabIndex = 9;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(3, 421);
+            button11.Name = "button11";
+            button11.Size = new Size(254, 37);
+            button11.TabIndex = 10;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(3, 464);
+            button12.Name = "button12";
+            button12.Size = new Size(254, 36);
+            button12.TabIndex = 11;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(3, 506);
+            button13.Name = "button13";
+            button13.Size = new Size(254, 36);
+            button13.TabIndex = 12;
+            button13.Text = "button13";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(3, 548);
+            button14.Name = "button14";
+            button14.Size = new Size(254, 36);
+            button14.TabIndex = 13;
+            button14.Text = "button14";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(3, 590);
+            button15.Name = "button15";
+            button15.Size = new Size(254, 36);
+            button15.TabIndex = 14;
+            button15.Text = "button15";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(3, 632);
+            button16.Name = "button16";
+            button16.Size = new Size(254, 36);
+            button16.TabIndex = 15;
+            button16.Text = "button16";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Gainsboro;
+            panel3.Location = new Point(307, 187);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(651, 519);
+            panel3.TabIndex = 2;
+            // 
+            // InterfazGerenteMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightBlue;
+            ClientSize = new Size(961, 720);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "InterfazGerenteMenu";
+            Text = "InterfazGerenteMenu";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+    }
+}
---
+++ SisteamasVentas/SistemasVentas.VISTA/InterfazGerenteVista/InterfazGerenteMenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemasVentas.VISTA.InterfazGerenteVista
+{
+    public partial class InterfazGerenteMenu : Form
+    {
+        public InterfazGerenteMenu()
+        {
+            InitializeComponent();
+            // Configuración del tamaño del FlowLayoutPanel
+            flowLayoutPanel1.Width = panel1.ClientSize.Width; // Ancho igual al del panel
+            flowLayoutPanel1.AutoSize = true; // Para ajustar automáticamente la altura
+
+            // Configuración del Panel para desplazamiento automático
+            panel1.AutoScroll = true;
+
+            // Agregar botones al FlowLayoutPanel
+            for (int i = 0; i < 20; i++)
+            {
+                Button button = new Button();
+                button.Text = "Botón " + (i + 1);
+                button.Width = flowLayoutPanel1.ClientSize.Width - SystemInformation.VerticalScrollBarWidth; // Ajustar ancho para tener en cuenta la barra de desplazamiento vertical
+                flowLayoutPanel1.Controls.Add(button);
+            }
+        }
+    }
+}
